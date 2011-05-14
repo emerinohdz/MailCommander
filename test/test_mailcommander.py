b2ac8dc76a94b8b9770acb84b6836c41834c321b
Edgar Merino <donvodka@gmail.com>
2011-05-13 20:42:23 -0500

    updated tests

--- test/test_mailcommander.py
+++ test/test_mailcommander.py
@@ -2,9 +2,12 @@
 
 from unittest import TestCase, main
 
+from devpower.mail import UnicodeParser
+
 from scanner import MailScanner
 from auth import AuthManager
-from util import find_commands, Parsers
+from util import find_commands, Parsers, AuthNotification, SuccessNotification
+from notifications import EmailNotifier
 from commander import Commander
 
 class TestMailCommander(TestCase):
@@ -18,15 +21,43 @@ class TestMailCommander(TestCase):
         self.commander = Commander(self.auth)
         
         self.scanner = MailScanner(Parsers(self.commands))
-        self.email = open("test/data/cmd.email").read()
+        self.email = UnicodeParser().parse(open("test/data/cmd.email"))
+
+        self.notifier = EmailNotifier("10.2.1.2", 25)
 
     def test_commander(self):
-        cmd_id, data, authkey = self.scanner.scan(self.email)
+        cmd_id, data, sprops, authkey = self.scanner.scan(self.email)
 
         command = self.commands[cmd_id]
         self.commander.execute(command, data, authkey)
 
         print command.output["text"]
+
+    def test_auth_notification(self):
+        cmd_id, data, sprops, authkey = self.scanner.scan(self.email)
+        command = self.commands[cmd_id]
+
+        self.commander.execute(command, data, authkey)
+
+        sender = "test@example.com"
+        recipients = ["emerino@etesa.com.mx"]
+
+        notification = AuthNotification(cmd_id, sender, recipients,
+                                        self.email, data)
+
+        self.notifier.send(notification)
+
+    def test_success_notification(self):
+        cmd_id, data, sprops, authkey = self.scanner.scan(self.email)
+        command = self.commands[cmd_id]
+
+        self.commander.execute(command, data, authkey)
+
+        sender = "test@example.com"
+        recipients = []
+
+        notification = SuccessNotification(sender, recipients, command, sprops)
+        self.notifier.send(notification)
 
 if __name__ == "__main__":
     main()
--- test/test_notifications.py
+++ test/test_notifications.py
@@ -2,41 +2,23 @@
 
 from unittest import TestCase, main
 
-from devpower.mail import UnicodeParser
-from notifications import Notifier
-from auth import AuthKey
-from util import find_commands
+from notifications import EmailNotifier, Notification
 
 class TestNotifications(TestCase):
     
     def __init__(self, name):
         TestCase.__init__(self, name)
+
+        self.notifier = EmailNotifier("10.2.1.2", 25)
         
-        commands = find_commands("test/cmds/")
-        self.command = commands["test"]
-
-        conf = {
-            "notification.sender": "sysadmin@etesa.com.mx",
-            "commander.sender": "supercorreo@etesa.com.mx",
-            "lang": "es"
-        }
-
-        keys = {"test": [AuthKey("emerino@etesa.com.mx", None)]}
-        self.notifier = Notifier(conf, keys)
-
-        self.email = UnicodeParser().parse(open("test/data/cmd.email"))
-
-#    def test_auth_notification(self):
-#        self.notifier.send_auth(self.command, self.email)
-
-#    def test_auth_success(self):
-#        data = {"subject": "MIRA ASUNTO", "key2": "something else"}
-#        print data["subject"]
-
-#        self.notifier.send_success(self.command, self.command.execute(data)["data"])
-
-    def test_error(self):
-        self.notifier.send_error("test", self.email, "UN ERROR!")
+    def test_notification(self):
+        notification = Notification()
+        notification.title = "Test"
+        notification.sender = "test@example.com"
+        notification.recipients.append("emerino@etesa.com.mx")
+        notification.body["text"] = "JOJOJOJO"
+           
+        self.notifier.send(notification)
 
 if __name__ == "__main__":
     main()
